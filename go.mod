0569cf6a7de0bff7da120c313210fbe3989a63ee
LY <1077607159@qq.com>
2024-08-27 21:24:12 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,18 @@
+module pro
+
+go 1.21.6
+
+require (
+	github.com/go-ping/ping v1.1.0
+	google.golang.org/grpc v1.65.0
+	google.golang.org/protobuf v1.34.2
+)
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240528184218-531527333157 // indirect
+)
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"pro/pb"
+	"strconv"
+	"time"
+
+	"github.com/go-ping/ping"
+	"google.golang.org/grpc"
+)
+
+var (
+	// 需要改为所有需要测试节点的IP地址
+	targetIP = []string{"10.129.32.84", "10.129.13.55", "10.129.173.253", "10.112.42.129"}
+	address  = "10.112.42.129:30030" // zookeeper地址
+	// 需要改为发出ping测试命令的节点IP，同样是要部署的节点IP
+)
+
+// node1, node2, node3, node4
+
+func main() {
+
+	// 每隔10秒ping一次
+	for {
+		for index, ip := range targetIP {
+			// 创建ping对象
+			pinger, err := ping.NewPinger(ip)
+			if err != nil {
+				fmt.Printf("无法创建Pinger: %v\n", err)
+				return
+			}
+
+			// 设置为ipv4
+			pinger.SetPrivileged(true)
+			// 设置Ping次数为1次
+			pinger.Count = 1
+
+			// 运行ping操作
+			err = pinger.Run()
+			if err != nil {
+				fmt.Printf("Ping失败: %v\n", err)
+			} else {
+				stats := pinger.Statistics()
+				// fmt.Printf("Nuc X Ping Nuc %v: 延迟=%v 丢包率=%.2f%%\n", index+1, stats.AvgRtt, stats.PacketLoss)
+				//建立链接
+				conn, err := grpc.Dial(address, grpc.WithInsecure())
+				if err != nil {
+					log.Fatalf("did not connect: %v", err)
+				}
+				defer conn.Close()
+				c := pb.NewZkServiceClient(conn)
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+				defer cancel()
+				_, err = c.Set(ctx, &pb.PathAndData{Path: "/latency/node4-node" + strconv.Itoa(index+1), Data: stats.AvgRtt.String()})
+				if err != nil {
+					log.Printf("/latency/node4-node%v写入错误: %v", index+1, err)
+				}
+			}
+		}
+		// 等待10秒再进行下一次ping
+		time.Sleep(10 * time.Second)
+	}
+}
