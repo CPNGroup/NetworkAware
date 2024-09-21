e3c157451a1cb2a44f072eeff022b1a8af3f457d
“CPNGroup” <ly209991@bupt.edu.cn>
2024-09-21 09:41:50 +0800

    0921

--- main.go
+++ main.go
@@ -2,26 +2,20 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
+	"pro/networkaware"
 	"pro/pb"
-	"strconv"
 	"time"
 
-	"github.com/go-ping/ping"
 	"google.golang.org/grpc"
 )
 
 var (
-	// 需要改为所有需要测试节点的IP地址
-	targetIP = []string{"10.129.32.84", "10.129.13.55", "10.129.173.253", "10.129.184.238"}
-	address  = "10.129.184.238:30030" // zookeeper地址
+	address = "10.129.32.84:30030" // zookeeper地址
 	// 需要改为发出ping测试命令的节点IP，同样是要部署的节点IP
 )
 
-// node1, node2, node3, node4
-
-func main() {
+func WriteIn() {
 	//建立链接
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
@@ -30,37 +24,24 @@ func main() {
 	defer conn.Close()
 	c := pb.NewZkServiceClient(conn)
 
-	// 每隔30秒ping一次
-	for {
-		for index, ip := range targetIP {
-			// 创建ping对象
-			pinger, err := ping.NewPinger(ip)
-			if err != nil {
-				fmt.Printf("无法创建Pinger: %v\n", err)
-				return
-			}
-
-			// 设置为ipv4
-			pinger.SetPrivileged(true)
-			// 设置Ping次数为1次
-			pinger.Count = 1
-
-			// 运行ping操作
-			err = pinger.Run()
-			if err != nil {
-				fmt.Printf("Ping失败: %v\n", err)
-			} else {
-				stats := pinger.Statistics()
-				// fmt.Printf("Nuc X Ping Nuc %v: 延迟=%v 丢包率=%.2f%%\n", index+1, stats.AvgRtt, stats.PacketLoss)
-				ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-				defer cancel()
-				_, err = c.Set(ctx, &pb.PathAndData{Path: "/latency/node4-node" + strconv.Itoa(index+1), Data: stats.AvgRtt.String()})
-				if err != nil {
-					log.Printf("/latency/node4-node%v写入错误: %v", index+1, err)
-				}
-			}
+	latency := networkaware.GetLatency()
+	for key, value := range latency {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		_, err = c.Set(ctx, &pb.PathAndData{Path: key, Data: value})
+		if err != nil {
+			log.Printf("latency写入错误: %v", err)
 		}
-		// 等待30秒再进行下一次ping
+
+	}
+}
+
+func main() {
+	// 每隔30秒执行一次
+	for {
+		WriteIn()
+
+		// 等待30秒
 		time.Sleep(30 * time.Second)
 	}
 }
---
+++ networkaware/network.go
@@ -0,0 +1,37 @@
+package networkaware
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/go-ping/ping"
+)
+
+func GetLatency() map[string]string {
+	LatencyMap := make(map[string]string)
+	// 需要改为所有需要测试节点的IP地址
+	targetIP := []string{"10.129.32.84", "10.129.13.55", "10.129.173.253", "10.129.184.238"}
+	for index, ip := range targetIP {
+		// 创建ping对象
+		pinger, err := ping.NewPinger(ip)
+		if err != nil {
+			fmt.Printf("无法创建Pinger: %v\n", err)
+			return nil
+		}
+
+		// 设置为ipv4
+		pinger.SetPrivileged(true)
+		// 设置Ping次数为1次
+		pinger.Count = 1
+
+		// 运行ping操作
+		err = pinger.Run()
+		if err != nil {
+			fmt.Printf("Ping失败: %v\n", err)
+		} else {
+			stats := pinger.Statistics()
+			LatencyMap["/latency/node1-node"+strconv.Itoa(index+1)] = stats.AvgRtt.String()
+		}
+	}
+	return LatencyMap
+}
